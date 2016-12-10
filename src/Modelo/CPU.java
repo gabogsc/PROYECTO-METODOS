14694fe97f5b218494579fc2830f599d4e0009eb
Carlos <Carlos@DESKTOP-MA0EH7N>
2016-12-10 20:47:04 -0300

    El gabo la come 2

--- src/Modelo/CPU.java
+++ src/Modelo/CPU.java
@@ -1,12 +1,17 @@
 
 package Modelo;
 
+import java.util.ArrayList;
+
 
 public class CPU {
     
 
 
     // METODOS
-    public void movimientoCPU(Personaje personaje, int x, int y){}
+    public void movimientoCPU(Personaje personaje, int x, int y){
+        
+    
+    }
     public void ataqueCPU(Personaje personaje, int x, int y){}
 }
